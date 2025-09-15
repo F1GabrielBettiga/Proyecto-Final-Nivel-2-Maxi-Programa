e079668110c4a4417b1cfa09047082fa2922ae12
GabrielBettiga <bettgas468@gmail.com>
2025-09-15 20:16:06 -0300

    implementar clase AccesoDatos con conexión, comandos y métodos básicos
    Se agregó la clase AccesoDatos en la capa Negocio, con la siguiente funcionalidad:
    
    Constructor: inicializa la conexión y el comando.
    
    Lector: propiedad de solo lectura para obtener el SqlDataReader.
    
    setearConsulta: asigna el texto de la consulta al comando.
    
    ejecutarLectura: abre conexión y ejecuta un SELECT devolviendo un SqlDataReader.
    
    ejecutarAccion: ejecuta sentencias INSERT, UPDATE o DELETE.
    
    setearParametro: agrega parámetros al comando.
    
    cerrarConexion: cierra el lector (si existe) y la conexión.

--- TPFinalNivel2_BettigaGustavoGabriel/Negocio/AccesoDatos.cs
+++ TPFinalNivel2_BettigaGustavoGabriel/Negocio/AccesoDatos.cs
@@ -3,10 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Negocio
 {
     public class AccesoDatos
     {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_DB; integrated security=true");
+            comando = new SqlCommand();
+
+        }
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+
+
+        }
+
+        //Ejecuta la lectura de la consulta seteada
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //Ejecuta una accion (insert, delete, update)
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
     }
 }
