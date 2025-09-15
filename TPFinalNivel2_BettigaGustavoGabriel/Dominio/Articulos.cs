940ece00e7e681b10bed454e373ead80ef5d2439
GabrielBettiga <bettgas468@gmail.com>
2025-09-15 17:10:04 -0300

    Creación de Clases
    Creación de Clases

---
+++ TPFinalNivel2_BettigaGustavoGabriel/Dominio/Articulos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Articulos
+    {
+        public int id { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int idMarca { get; set; }
+        public int idcategoria { get; set; }
+        public string imagenUrl { get; set; }
+        public decimal precio { get; set; }
+    }
+}
---
+++ TPFinalNivel2_BettigaGustavoGabriel/Dominio/Marcas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Marcas
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+
+
+    }
+}
