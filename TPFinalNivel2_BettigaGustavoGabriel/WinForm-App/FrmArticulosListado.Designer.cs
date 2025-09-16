368013d5667598b1ba0b8f36f3870d114303b0ae
GabrielBettiga <bettgas468@gmail.com>
2025-09-15 22:07:44 -0300

    cargar grilla al iniciar formulario y mostrar imagen al seleccionar artículo
    Se agregaron y ajustaron funcionalidades en el formulario de listado de artículos:
    
    Carga inicial: al iniciar el WinForm se ejecuta el método que carga la grilla con los artículos obtenidos desde la base de datos.
    
    Selección en grilla: al seleccionar un artículo en la grilla, se muestra su imagen en el PictureBox.
    
    Imagen inicial: al cargar el formulario, el PictureBox muestra la imagen correspondiente al primer artículo.
    
    Sin imagen: si no hay un artículo seleccionado o el artículo no tiene URL de imagen, el PictureBox muestra una imagen por defecto indicando que no hay foto disponible.
    
    DisplayName: en la clase Articulo se configuraron los DisplayName para que las columnas de la grilla se muestren con nombres claros y entendibles.
    
    Ajuste dinámico de la grilla: el DataGridView adapta su tamaño según la cantidad de artículos cargados; si la lista sobrepasa el tamaño máximo definido, aparece el scroll vertical.
    
    De esta forma, el listado es más dinámico, claro y visualmente consistente, mostrando siempre información válida y evitando errores al no tener imagen.

--- TPFinalNivel2_BettigaGustavoGabriel/WinForm-App/FrmArticulosListado.Designer.cs
+++ TPFinalNivel2_BettigaGustavoGabriel/WinForm-App/FrmArticulosListado.Designer.cs
@@ -43,21 +43,32 @@
             this.lblBuscarEn = new System.Windows.Forms.Label();
             this.lblOperador = new System.Windows.Forms.Label();
             this.lblValor = new System.Windows.Forms.Label();
+            this.pnlFiltroAvanzado = new System.Windows.Forms.Panel();
+            this.pnlBotones = new System.Windows.Forms.Panel();
+            this.pnlFiltroRapido = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvListadoArticulos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbImagenListado)).BeginInit();
+            this.pnlFiltroAvanzado.SuspendLayout();
+            this.pnlBotones.SuspendLayout();
+            this.pnlFiltroRapido.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvListadoArticulos
             // 
+            this.dgvListadoArticulos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvListadoArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListadoArticulos.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.dgvListadoArticulos.Location = new System.Drawing.Point(12, 48);
+            this.dgvListadoArticulos.MultiSelect = false;
             this.dgvListadoArticulos.Name = "dgvListadoArticulos";
+            this.dgvListadoArticulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvListadoArticulos.Size = new System.Drawing.Size(548, 315);
             this.dgvListadoArticulos.TabIndex = 0;
+            this.dgvListadoArticulos.SelectionChanged += new System.EventHandler(this.dgvListadoArticulos_SelectionChanged);
             // 
             // txtbFiltroRapido
             // 
-            this.txtbFiltroRapido.Location = new System.Drawing.Point(370, 22);
+            this.txtbFiltroRapido.Location = new System.Drawing.Point(72, 6);
             this.txtbFiltroRapido.Name = "txtbFiltroRapido";
             this.txtbFiltroRapido.Size = new System.Drawing.Size(190, 20);
             this.txtbFiltroRapido.TabIndex = 1;
@@ -65,7 +76,7 @@
             // lblFiltroRapido
             // 
             this.lblFiltroRapido.AutoSize = true;
-            this.lblFiltroRapido.Location = new System.Drawing.Point(295, 25);
+            this.lblFiltroRapido.Location = new System.Drawing.Point(-3, 9);
             this.lblFiltroRapido.Name = "lblFiltroRapido";
             this.lblFiltroRapido.Size = new System.Drawing.Size(69, 13);
             this.lblFiltroRapido.TabIndex = 2;
@@ -73,7 +84,7 @@
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(12, 369);
+            this.btnAgregar.Location = new System.Drawing.Point(3, 12);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
             this.btnAgregar.TabIndex = 3;
@@ -83,7 +94,7 @@
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(12, 427);
+            this.btnEliminar.Location = new System.Drawing.Point(3, 70);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(75, 23);
             this.btnEliminar.TabIndex = 4;
@@ -92,7 +103,7 @@
             // 
             // btnModificar
             // 
-            this.btnModificar.Location = new System.Drawing.Point(12, 398);
+            this.btnModificar.Location = new System.Drawing.Point(3, 41);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(75, 23);
             this.btnModificar.TabIndex = 5;
@@ -102,15 +113,15 @@
             // 
             // pbImagenListado
             // 
-            this.pbImagenListado.Location = new System.Drawing.Point(595, 48);
+            this.pbImagenListado.Location = new System.Drawing.Point(616, 48);
             this.pbImagenListado.Name = "pbImagenListado";
-            this.pbImagenListado.Size = new System.Drawing.Size(300, 300);
+            this.pbImagenListado.Size = new System.Drawing.Size(269, 315);
             this.pbImagenListado.TabIndex = 6;
             this.pbImagenListado.TabStop = false;
             // 
             // btnDetalles
             // 
-            this.btnDetalles.Location = new System.Drawing.Point(93, 390);
+            this.btnDetalles.Location = new System.Drawing.Point(84, 33);
             this.btnDetalles.Name = "btnDetalles";
             this.btnDetalles.Size = new System.Drawing.Size(75, 39);
             this.btnDetalles.TabIndex = 7;
@@ -121,7 +132,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(439, 390);
+            this.comboBox1.Location = new System.Drawing.Point(76, 22);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 8;
@@ -129,14 +140,14 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(439, 417);
+            this.comboBox2.Location = new System.Drawing.Point(76, 49);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 9;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(439, 444);
+            this.textBox1.Location = new System.Drawing.Point(76, 76);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(121, 20);
             this.textBox1.TabIndex = 10;
@@ -144,7 +155,7 @@
             // lblFiltroAvanzado
             // 
             this.lblFiltroAvanzado.AutoSize = true;
-            this.lblFiltroAvanzado.Location = new System.Drawing.Point(459, 369);
+            this.lblFiltroAvanzado.Location = new System.Drawing.Point(96, 1);
             this.lblFiltroAvanzado.Name = "lblFiltroAvanzado";
             this.lblFiltroAvanzado.Size = new System.Drawing.Size(80, 13);
             this.lblFiltroAvanzado.TabIndex = 11;
@@ -153,7 +164,7 @@
             // lblBuscarEn
             // 
             this.lblBuscarEn.AutoSize = true;
-            this.lblBuscarEn.Location = new System.Drawing.Point(375, 393);
+            this.lblBuscarEn.Location = new System.Drawing.Point(12, 25);
             this.lblBuscarEn.Name = "lblBuscarEn";
             this.lblBuscarEn.Size = new System.Drawing.Size(58, 13);
             this.lblBuscarEn.TabIndex = 12;
@@ -162,7 +173,7 @@
             // lblOperador
             // 
             this.lblOperador.AutoSize = true;
-            this.lblOperador.Location = new System.Drawing.Point(375, 420);
+            this.lblOperador.Location = new System.Drawing.Point(12, 52);
             this.lblOperador.Name = "lblOperador";
             this.lblOperador.Size = new System.Drawing.Size(54, 13);
             this.lblOperador.TabIndex = 13;
@@ -171,39 +182,68 @@
             // lblValor
             // 
             this.lblValor.AutoSize = true;
-            this.lblValor.Location = new System.Drawing.Point(395, 447);
+            this.lblValor.Location = new System.Drawing.Point(32, 79);
             this.lblValor.Name = "lblValor";
             this.lblValor.Size = new System.Drawing.Size(34, 13);
             this.lblValor.TabIndex = 14;
             this.lblValor.Text = "Valor:";
+            // 
+            // pnlFiltroAvanzado
+            // 
+            this.pnlFiltroAvanzado.Controls.Add(this.comboBox2);
+            this.pnlFiltroAvanzado.Controls.Add(this.lblValor);
+            this.pnlFiltroAvanzado.Controls.Add(this.comboBox1);
+            this.pnlFiltroAvanzado.Controls.Add(this.lblOperador);
+            this.pnlFiltroAvanzado.Controls.Add(this.textBox1);
+            this.pnlFiltroAvanzado.Controls.Add(this.lblBuscarEn);
+            this.pnlFiltroAvanzado.Controls.Add(this.lblFiltroAvanzado);
+            this.pnlFiltroAvanzado.Location = new System.Drawing.Point(360, 369);
+            this.pnlFiltroAvanzado.Name = "pnlFiltroAvanzado";
+            this.pnlFiltroAvanzado.Size = new System.Drawing.Size(200, 96);
+            this.pnlFiltroAvanzado.TabIndex = 15;
+            // 
+            // pnlBotones
+            // 
+            this.pnlBotones.Controls.Add(this.btnModificar);
+            this.pnlBotones.Controls.Add(this.btnAgregar);
+            this.pnlBotones.Controls.Add(this.btnDetalles);
+            this.pnlBotones.Controls.Add(this.btnEliminar);
+            this.pnlBotones.Location = new System.Drawing.Point(12, 370);
+            this.pnlBotones.Name = "pnlBotones";
+            this.pnlBotones.Size = new System.Drawing.Size(200, 95);
+            this.pnlBotones.TabIndex = 16;
+            // 
+            // pnlFiltroRapido
+            // 
+            this.pnlFiltroRapido.Controls.Add(this.txtbFiltroRapido);
+            this.pnlFiltroRapido.Controls.Add(this.lblFiltroRapido);
+            this.pnlFiltroRapido.Location = new System.Drawing.Point(287, 12);
+            this.pnlFiltroRapido.Name = "pnlFiltroRapido";
+            this.pnlFiltroRapido.Size = new System.Drawing.Size(273, 29);
+            this.pnlFiltroRapido.TabIndex = 17;
             // 
             // FrmArticulosListado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(897, 469);
-            this.Controls.Add(this.lblValor);
-            this.Controls.Add(this.lblOperador);
-            this.Controls.Add(this.lblBuscarEn);
-            this.Controls.Add(this.lblFiltroAvanzado);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.btnDetalles);
+            this.Controls.Add(this.pnlFiltroRapido);
+            this.Controls.Add(this.pnlBotones);
+            this.Controls.Add(this.pnlFiltroAvanzado);
             this.Controls.Add(this.pbImagenListado);
-            this.Controls.Add(this.btnModificar);
-            this.Controls.Add(this.btnEliminar);
-            this.Controls.Add(this.btnAgregar);
-            this.Controls.Add(this.lblFiltroRapido);
-            this.Controls.Add(this.txtbFiltroRapido);
             this.Controls.Add(this.dgvListadoArticulos);
             this.Name = "FrmArticulosListado";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Articulos";
+            this.Load += new System.EventHandler(this.FrmArticulosListado_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvListadoArticulos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbImagenListado)).EndInit();
+            this.pnlFiltroAvanzado.ResumeLayout(false);
+            this.pnlFiltroAvanzado.PerformLayout();
+            this.pnlBotones.ResumeLayout(false);
+            this.pnlFiltroRapido.ResumeLayout(false);
+            this.pnlFiltroRapido.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -224,5 +264,8 @@
         private System.Windows.Forms.Label lblBuscarEn;
         private System.Windows.Forms.Label lblOperador;
         private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Panel pnlFiltroAvanzado;
+        private System.Windows.Forms.Panel pnlBotones;
+        private System.Windows.Forms.Panel pnlFiltroRapido;
     }
 }
--- TPFinalNivel2_BettigaGustavoGabriel/WinForm-App/FrmArticulosListado.cs
+++ TPFinalNivel2_BettigaGustavoGabriel/WinForm-App/FrmArticulosListado.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,18 @@ namespace WinForm_App
 {
     public partial class FrmArticulosListado : Form
     {
+        private List<Articulo> listaArticulos;
         public FrmArticulosListado()
         {
             InitializeComponent();
+        }
+
+
+        private void FrmArticulosListado_Load(object sender, EventArgs e)
+        {
+            CargarGrilla();
+            modificarAltoDeFilasDGV(dgvListadoArticulos, 315);
+
         }
 
         private void btnAgregar_Click(object sender, EventArgs e)
@@ -37,5 +49,77 @@ namespace WinForm_App
             Detalles.Text = "Detalles del Artículo";
             Detalles.ShowDialog();
         }
+
+        private void dgvListadoArticulos_SelectionChanged(object sender, EventArgs e)
+        {
+
+            if (dgvListadoArticulos.CurrentRow != null)
+            {
+                Articulo seleccionado = (Articulo)dgvListadoArticulos.CurrentRow.DataBoundItem;
+                cargarImagen(seleccionado.imagenUrl);
+            }
+
+        }
+
+        private void CargarGrilla()
+        {
+            dgvListadoArticulos.DataSource = null;
+            ArticuloNegocio negocio = new ArticuloNegocio();
+
+            try
+            {
+                listaArticulos = negocio.listarTodosLosArticulos();
+
+                dgvListadoArticulos.DataSource = listaArticulos;
+                ocultarColumnas();
+                cargarImagen(listaArticulos[0].imagenUrl);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+
+
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbImagenListado.Load(imagen);
+                pbImagenListado.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            catch (Exception)
+            {
+                pbImagenListado.Load("https://efectocolibri.com/wp-content/uploads/2021/01/placeholder.png");
+                pbImagenListado.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        
+        }
+
+        private void ocultarColumnas()
+        {
+            dgvListadoArticulos.Columns["Id"].Visible = false;
+            dgvListadoArticulos.Columns["ImagenUrl"].Visible = false;
+        }
+
+        private void modificarAltoDeFilasDGV(DataGridView dgv, int altoMaximo)
+        {
+            int header = dgv.ColumnHeadersVisible ? dgv.ColumnHeadersHeight : 0;
+            int borders = dgv.Height - dgv.ClientSize.Height;
+            int altoFila = dgv.RowTemplate.Height;
+
+            // Calcular alto en base a filas cargadas
+            int filas = dgv.Rows.Count;
+            int altoDeseado = header + (filas * altoFila) + borders;
+
+            // Asignar el alto, respetando el máximo
+            dgv.Height = Math.Min(altoDeseado, altoMaximo);
+        }
+
+
     }
 }
